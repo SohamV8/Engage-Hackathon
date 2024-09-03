62a82511c4f9405b79a5dc4a4ac70fa74805cd8c
unknown <sohamvashists8@gmail.com>
2024-09-04 01:08:57 +0530

    main

--- React/Design1/src/Components/Sponsors.jsx
+++ React/Design1/src/Components/Sponsors.jsx
@@ -1,32 +1,74 @@
 import React from 'react';
 import './Sponsors.css';
-import AnimatedTitle from './AnimatedTitle'; // Assuming AnimatedTitle is in the same directory
+import AnimatedTitle from './AnimatedTitle';
+
+// Import sponsor images from assets
+import devfolio from '../assets/devfolio.png';
+import poly from '../assets/poly.png';
+import woldram from '../assets/woldram.png';
+import bee from '../assets/bee.png';
+import sticker from '../assets/sticker.png';
+import echo from '../assets/echo.png';
+import rosen from '../assets/rosen.webp';
+import replit from '../assets/replit.png';
+import gdsc1 from '../assets/gdsc1.png';
+import gdg from '../assets/gdg.png';
+import gdscncut from '../assets/gdscncut.png';
 
 function Sponsors() {
   return (
     <div id='Sponsors'>
       <AnimatedTitle text="SPONSORS" />
       <div className="sponsors-flex">
-      <div className="sponsorship">
-        <AnimatedTitle text="Gold Sponsors" />
-        <img src={devfolio} alt="Gold Sponsor 1" />
-        <img src={poly} alt="Gold Sponsor 2" />
-      </div>
-      <div className="sponsorship">
-        <AnimatedTitle text="Silver Sponsors" />
-        <img src={woldram} alt="Silver Sponsor 1" />
-        <img src={bee} alt="Silver Sponsor 2" />
-        <img src={sticker} alt="Silver Sponsor 3" />
-        <img src={echo} alt="Silver Sponsor 4" />
-        <img src={rosen} alt="Silver Sponsor 5" />
-        <img src={replit} alt="Silver Sponsor 6" />
-      </div>
-      <div className="sponsorship">
-        <AnimatedTitle text="Community Partners" />
-        <img src={gdsc1} alt="Community Partner 1" />
-        <img src={gdg} alt="Community Partner 2" />
-        <img src={gdscncut} alt="Community Partner 3" />
-      </div>
+        
+        <div className="sponsorship">
+          <AnimatedTitle text="Gold Sponsors" />
+          <div className="images-sponsors">
+          <a href="https://devfolio.co/" target="_blank" rel="noopener noreferrer">
+            <img src={devfolio} alt="Gold Sponsor 1" />
+          </a>
+          <a href="https://www.polygon.technology/" target="_blank" rel="noopener noreferrer">
+            <img src={poly} alt="Gold Sponsor 2" />
+          </a>
+        </div>
+        </div>
+        <div className="sponsorship">
+          <AnimatedTitle text="Silver Sponsors" />
+          <div className="images-sponsors">
+          <a href="https://www.wolfram.com/" target="_blank" rel="noopener noreferrer">
+            <img src={woldram} alt="Silver Sponsor 1" />
+          </a>
+          <a href="https://beeceptor.com/" target="_blank" rel="noopener noreferrer">
+            <img src={bee} alt="Silver Sponsor 2" />
+          </a>
+          <a href="https://www.stickermule.com/" target="_blank" rel="noopener noreferrer">
+            <img src={sticker} alt="Silver Sponsor 3" />
+          </a>
+          <a href="https://www.echo3d.com/" target="_blank" rel="noopener noreferrer">
+            <img src={echo} alt="Silver Sponsor 4" />
+          </a>
+          <a href="https://rosenfeldmedia.com/" target="_blank" rel="noopener noreferrer">
+            <img src={rosen} alt="Silver Sponsor 5" />
+          </a>
+          <a href="https://replit.com/" target="_blank" rel="noopener noreferrer">
+            <img src={replit} alt="Silver Sponsor 6" />
+          </a>
+        </div>
+        </div>
+        <div className="sponsorship">
+          <AnimatedTitle text="Community Partners" />
+          <div className="images-sponsors">
+          <a href="$" target="_blank" rel="noopener noreferrer">
+            <img src={gdsc1} alt="Community Partner 1" />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src={gdg} alt="Community Partner 2" />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src={gdscncut} alt="Community Partner 3" />
+          </a>
+        </div>
+</div>
       </div>
     </div>
   );
