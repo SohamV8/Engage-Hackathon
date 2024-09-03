811eb37053c664225b2e88c19f114fd5701e80b3
unknown <sohamvashists8@gmail.com>
2024-09-04 00:54:04 +0530

    main

--- React/Design1/src/Components/Sponsors.jsx
+++ React/Design1/src/Components/Sponsors.jsx
@@ -9,23 +9,23 @@ function Sponsors() {
       <div className="sponsors-flex">
       <div className="sponsorship">
         <AnimatedTitle text="Gold Sponsors" />
-        <img src="" alt="Gold Sponsor 1" />
-        <img src="" alt="Gold Sponsor 2" />
+        <img src={devfolio} alt="Gold Sponsor 1" />
+        <img src={poly} alt="Gold Sponsor 2" />
       </div>
       <div className="sponsorship">
         <AnimatedTitle text="Silver Sponsors" />
-        <img src="" alt="Silver Sponsor 1" />
-        <img src="" alt="Silver Sponsor 2" />
-        <img src="" alt="Silver Sponsor 3" />
-        <img src="" alt="Silver Sponsor 4" />
-        <img src="" alt="Silver Sponsor 5" />
-        <img src="" alt="Silver Sponsor 6" />
+        <img src={woldram} alt="Silver Sponsor 1" />
+        <img src={bee} alt="Silver Sponsor 2" />
+        <img src={sticker} alt="Silver Sponsor 3" />
+        <img src={echo} alt="Silver Sponsor 4" />
+        <img src={rosen} alt="Silver Sponsor 5" />
+        <img src={replit} alt="Silver Sponsor 6" />
       </div>
       <div className="sponsorship">
         <AnimatedTitle text="Community Partners" />
-        <img src="" alt="Community Partner 1" />
-        <img src="" alt="Community Partner 2" />
-        <img src="" alt="Community Partner 3" />
+        <img src={gdsc1} alt="Community Partner 1" />
+        <img src={gdg} alt="Community Partner 2" />
+        <img src={gdscncut} alt="Community Partner 3" />
       </div>
       </div>
     </div>
